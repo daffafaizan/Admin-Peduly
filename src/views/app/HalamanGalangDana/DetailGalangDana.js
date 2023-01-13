7fef7f3ca333b19226e7b8241791a3fd3034b850
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-01-13 09:56:14 +0700

    slicing

--- src/views/app/HalamanGalangDana/DetailGalangDana.js
+++ src/views/app/HalamanGalangDana/DetailGalangDana.js
@@ -5,24 +5,24 @@ import {
   Card,
   CardBody,
   Table,
-  UncontrolledDropdown,
-  DropdownMenu,
-  DropdownItem,
-  DropdownToggle,
+  // UncontrolledDropdown,
+  // DropdownMenu,
+  // DropdownItem,
+  // DropdownToggle,
 } from 'reactstrap'
-import { Colxx, Separator } from 'components/common/CustomBootstrap'
-import IntlMessages from 'helpers/IntlMessages'
+import { Colxx } from 'components/common/CustomBootstrap'
+// import IntlMessages from 'helpers/IntlMessages'
 import useMousetrap from 'hooks/use-mousetrap'
 import { orderData } from 'helpers/Utils'
 import IdrFormat from 'helpers/IdrFormat'
 import DateFormat from 'helpers/DateFormat'
 import Breadcrumb from 'containers/navs/Breadcrumb'
 import { Link } from 'react-router-dom'
-import './index.css'
+import './index.scss'
 
-const orderOptions = [{ label: `Terbaru` }, { label: `Terlama` }]
+// const orderOptions = [{ label: `Terbaru` }, { label: `Terlama` }]
 
-const pageSizes = [4, 8, 12, 20]
+// const pageSizes = [4, 8, 12, 20]
 
 const galangDana = [
   {
@@ -64,19 +64,19 @@ const initialData = orderData('Terbaru', galangDana)
 
 const DetailGalangDana = ({ match }) => {
   const [selectedItems, setSelectedItems] = useState([])
-  const [selectedOrder, setSelectedOrder] = useState('Terbaru')
-  const [data, setData] = useState(initialData)
-  const [search, setSearch] = useState('')
-  const handleChange = (e) => {
-    e.preventDefault()
-    setSearch(e.target.value)
-  }
+  // const [selectedOrder, setSelectedOrder] = useState('Terbaru')
+  const [data] = useState(initialData)
+  const [search] = useState('')
+  // const handleChange = (e) => {
+  //   e.preventDefault()
+  //   setSearch(e.target.value)
+  // }
 
-  const handleOrder = (option) => {
-    const array = orderData(option, initialData)
-    setData(array)
-    setSelectedOrder(option)
-  }
+  // const handleOrder = (option) => {
+  //   const array = orderData(option, initialData)
+  //   setData(array)
+  //   setSelectedOrder(option)
+  // }
 
   const handleChangeSelectAll = (isToggle) => {
     if (selectedItems.length >= initialData.length) {
@@ -104,252 +104,115 @@ const DetailGalangDana = ({ match }) => {
       <Row>
         <Colxx xxs="12">
           <Breadcrumb match={match} />
-          <Separator className="mb-3" />
+          {/* <Separator className="mb-3" /> */}
         </Colxx>
       </Row>
       <div className="d-flex" style={{ marginBottom: '38px' }}>
-        <div className="d-flex justify-content-beetwen">
+        <div className="d-flex w-full judul-container">
           <a
             href="https://peduly.com/donasi-sekali/bantubututi"
-            className="text-danger"
-            style={{ fontSize: '24px' }}
+            className="text-danger judul"
           >
             Alami Katarak,Bu Tuti jadi butuh bangunan demi pengobatan suami
           </a>
           <Link
             to="/error"
-            className="rounded border-status-danger text-danger"
-            style={{ padding: '10px 20px', fontSize: '14px' }}
+            className="rounded border-status-danger text-danger edit-btn"
           >
             Edit Galang Dana
           </Link>
         </div>
       </div>
       <Row>
-        <Colxx xxs="3">
-          <div className="card" style={{ borderRadius: '15px' }}>
-            <div className="d-block" style={{ padding: '50px' }}>
-              <svg
-                className="mx-auto w-full icon"
-                // style={{ width: '100%', marginBottom: '22px' }}
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                fill="none"
-                viewBox="0 0 60 60"
-              >
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="3"
-                  d="M5 21.25h31.25M15 41.25h5M26.25 41.25h10"
-                ></path>
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  d="M55 35.075v5.2c0 8.775-2.225 10.975-11.1 10.975H16.1C7.225 51.25 5 49.05 5 40.275v-20.55C5 10.95 7.225 8.75 16.1 8.75h20.15M50 8.75v15l5-5M50 23.75l-5-5"
-                ></path>
-              </svg>
-              <p
-                className="text-peduly-dark mx-auto text-center text"
-                // style={{
-                //   fontSize: '14px',
-                //   width: '100%',
-                //   marginBottom: '30px',
-                // }}
-              >
-                Dana Terkumpul
-              </p>
-              <p className="text-danger nominal">Rp78.050.000</p>
-            </div>
+        <Colxx xs="12" sm="6" lg="3">
+          <div className="card container-card">
+            <svg
+              className="mx-auto w-full icon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="60"
+              height="60"
+              fill="none"
+              viewBox="0 0 60 60"
+            >
+              <path
+                stroke="#E7513B"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+                strokeWidth="3"
+                d="M5 21.25h31.25M15 41.25h5M26.25 41.25h10"
+              ></path>
+              <path
+                stroke="#E7513B"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                d="M55 35.075v5.2c0 8.775-2.225 10.975-11.1 10.975H16.1C7.225 51.25 5 49.05 5 40.275v-20.55C5 10.95 7.225 8.75 16.1 8.75h20.15M50 8.75v15l5-5M50 23.75l-5-5"
+              ></path>
+            </svg>
+            <p className="text-peduly-dark mx-auto text-center judul">
+              Dana Terkumpul
+            </p>
+            <p className="text-danger text-center content">
+              Rp {IdrFormat(78050000)}
+            </p>
           </div>
         </Colxx>
-        <Colxx xxs="3">
-          <div className="card" style={{ borderRadius: '15px' }}>
-            <div className="d-block" style={{ padding: '50px' }}>
-              <svg
-                className="mx-auto"
-                style={{ width: '100%', marginBottom: '22px' }}
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                fill="none"
-                viewBox="0 0 60 60"
-              >
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="3"
-                  d="M5 21.25h31.25M15 41.25h5M26.25 41.25h10"
-                ></path>
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  d="M55 35.075v5.2c0 8.775-2.225 10.975-11.1 10.975H16.1C7.225 51.25 5 49.05 5 40.275v-20.55C5 10.95 7.225 8.75 16.1 8.75h20.15M50 8.75v15l5-5M50 23.75l-5-5"
-                ></path>
-              </svg>
-              <p
-                className="text-peduly-dark mx-auto text-center"
-                style={{
-                  fontSize: '14px',
-                  width: '100%',
-                  marginBottom: '30px',
-                }}
-              >
-                Dana Terkumpul
-              </p>
-              <p className="text-danger" style={{ fontSize: '28px' }}>
-                Rp78.050.000
-              </p>
-            </div>
+        <Colxx xs="12" sm="6" lg="3">
+          <div className="card container-card">
+            <svg
+              className="mx-auto w-full icon"
+              xmlns="http://www.w3.org/2000/svg"
+              width="60"
+              height="60"
+              fill="none"
+              viewBox="0 0 60 60"
+            >
+              <path
+                stroke="#E7513B"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="3"
+                d="M30 30c6.904 0 12.5-5.596 12.5-12.5S36.904 5 30 5s-12.5 5.596-12.5 12.5S23.096 30 30 30zM51.475 55c0-9.675-9.625-17.5-21.475-17.5-11.85 0-21.475 7.825-21.475 17.5"
+              ></path>
+            </svg>
+            <p className="text-peduly-dark mx-auto text-center judul">
+              Jumlah Donatur
+            </p>
+            <p className="text-danger text-center content">325 Donatur</p>
           </div>
         </Colxx>
-        <Colxx xxs="3">
-          <div className="card" style={{ borderRadius: '15px' }}>
-            <div className="d-block" style={{ padding: '50px' }}>
-              <svg
-                className="mx-auto"
-                style={{ width: '100%', marginBottom: '22px' }}
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                fill="none"
-                viewBox="0 0 60 60"
-              >
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="3"
-                  d="M5 21.25h31.25M15 41.25h5M26.25 41.25h10"
-                ></path>
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  d="M55 35.075v5.2c0 8.775-2.225 10.975-11.1 10.975H16.1C7.225 51.25 5 49.05 5 40.275v-20.55C5 10.95 7.225 8.75 16.1 8.75h20.15M50 8.75v15l5-5M50 23.75l-5-5"
-                ></path>
-              </svg>
-              <p
-                className="text-peduly-dark mx-auto text-center"
-                style={{
-                  fontSize: '14px',
-                  width: '100%',
-                  marginBottom: '30px',
-                }}
-              >
-                Dana Terkumpul
-              </p>
-              <p className="text-danger" style={{ fontSize: '28px' }}>
-                Rp78.050.000
-              </p>
-            </div>
-          </div>
+        <Colxx xs="12" sm="6" lg="3">
+          <Row className="row-gap-3">
+            <Colxx xxs="12">
+              <div className="card container-card-half card-top">
+                <p className="text-peduly-dark judul">Biaya Payment Gateway</p>
+                <p className="text-danger content">Rp {IdrFormat(50000)}</p>
+              </div>
+            </Colxx>
+            <Colxx xxs="12">
+              <div className="card container-card-half card-bottom">
+                <p className="text-peduly-dark judul">Biaya Operasional</p>
+                <p className="text-danger content">Rp {IdrFormat(23300000)}</p>
+              </div>
+            </Colxx>
+          </Row>
         </Colxx>
-        <Colxx xxs="3">
-          <div className="card" style={{ borderRadius: '15px' }}>
-            <div className="d-block" style={{ padding: '50px' }}>
-              <svg
-                className="mx-auto"
-                style={{ width: '100%', marginBottom: '22px' }}
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                fill="none"
-                viewBox="0 0 60 60"
-              >
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="3"
-                  d="M5 21.25h31.25M15 41.25h5M26.25 41.25h10"
-                ></path>
-                <path
-                  stroke="#E7513B"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  d="M55 35.075v5.2c0 8.775-2.225 10.975-11.1 10.975H16.1C7.225 51.25 5 49.05 5 40.275v-20.55C5 10.95 7.225 8.75 16.1 8.75h20.15M50 8.75v15l5-5M50 23.75l-5-5"
-                ></path>
-              </svg>
-              <p
-                className="text-peduly-dark mx-auto text-center"
-                style={{
-                  fontSize: '14px',
-                  width: '100%',
-                  marginBottom: '30px',
-                }}
-              >
-                Dana Terkumpul
-              </p>
-              <p className="text-danger" style={{ fontSize: '28px' }}>
-                Rp78.050.000
-              </p>
-            </div>
-          </div>
-        </Colxx>
-      </Row>
-      <Row>
-        <Colxx xxs="12" className="mb-3">
-          <div className="d-block d-md-inline-block pt-1">
-            <UncontrolledDropdown className="mr-1 float-md-left btn-group mb-1">
-              <DropdownToggle caret color="outline-dark" size="xs">
-                <IntlMessages id="pages.orderby" /> {selectedOrder}
-              </DropdownToggle>
-              <DropdownMenu>
-                {orderOptions.map((order, index) => {
-                  return (
-                    <DropdownItem
-                      key={index}
-                      onClick={() => handleOrder(order.label)}
-                    >
-                      {order.label}
-                    </DropdownItem>
-                  )
-                })}
-              </DropdownMenu>
-            </UncontrolledDropdown>
-            <div className="search-sm d-inline-block float-md-left mr-1 mb-1 align-top">
-              <input
-                type="text"
-                name="keyword"
-                id="search"
-                placeholder="Search..."
-                // onKeyPress={(e) => onSearchKey(e)}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-          <div className="float-md-right pt-1">
-            <span className="text-muted text-small mr-1">{`1 of 1 `}</span>
-            <UncontrolledDropdown className="d-inline-block">
-              <DropdownToggle caret color="outline-dark" size="xs">
-                {' '}
-                8{/* {selectedPageSize} */}
-              </DropdownToggle>
-              <DropdownMenu right>
-                {pageSizes.map((size, index) => {
-                  return (
-                    <DropdownItem key={index} onClick="">
-                      {size}
-                    </DropdownItem>
-                  )
-                })}
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </div>
+        <Colxx xs="12" sm="6" lg="3">
+          <Row className="row-gap-3">
+            <Colxx xxs="12">
+              <div className="card container-card-half card-top">
+                <p className="text-peduly-dark judul">Biaya Referal & Iklan</p>
+                <p className="text-danger content">Rp {IdrFormat(9300000)}</p>
+              </div>
+            </Colxx>
+            <Colxx xxs="12">
+              <div className="card container-card-half card-bottom">
+                <p className="text-peduly-dark judul">Total Payable</p>
+                <p className="text-danger content">Rp {IdrFormat(100300000)}</p>
+              </div>
+            </Colxx>
+          </Row>
         </Colxx>
       </Row>
       <Row>
